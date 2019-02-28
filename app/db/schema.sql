b5e749a510c0395878bd5ca224aba5934aedf93e
squall2046 <squall2046@hotmail.com>
2019-02-27 21:17:41 -0500

    update

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,8 +1,9 @@
-drop database if exists  friendfinder_db;
+-- drop database if exists  friendfinder_db;
 
-create database friendfinder_db;
-use friendfinder_db;
+-- create database friendfinder_db;
+-- use friendfinder_db;
 
+use [jawsdb database name, no square bracket];
 create table friends (
 id int primary key auto_increment not null,
 name varchar (50),
